02b347748744648e225c1245085185439443d2c0
jailsonrs <jailsonr@alu.ufc.br>
2025-09-18 22:13:48 -0300

    org

---
+++ src/agentic_cia/application/chat_sac_service/service.py
@@ -0,0 +1,30 @@
+from typing import Optional, List, Dict
+from agentic_cia.application.chat_sac_service.agent import qa_chain
+from agentic_cia.infrastructure.factory.chat_service_base import ChatService
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def print_formatted_response(response: str):
+    """
+    Recebe uma string e imprime no terminal com bullets e espaçamento.
+    Cada linha da string vira um item.
+    """
+    print("\nResposta do chat:\n")
+    for line in response.split("\n"):
+        line = line.strip()
+        if line:  # ignora linhas vazias
+            print(f"• {line}\n")
+
+
+class ChatSacService(ChatService):
+
+    def generate_response(self, message: str, history: Optional[List[Dict[str, str]]] = None) -> str:
+         logger.info(f"Pergunta recebida: {message}")
+         result = qa_chain.invoke(message)
+         print_formatted_response(result['result'])
+         return result['result']
+         #return result['result']
+
+        
